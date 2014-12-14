ce756ae4495b80945786577e761131577c71a1b8
David Roesel <roesel@gmail.com>
2014-12-14 18:33:41 +0100

    Moved jQuery to local file

--- index.php
+++ index.php
@@ -63,7 +63,8 @@ $region_array = array(
         <link rel='stylesheet' type='text/css' href='style.css'>
         <link rel='stylesheet' type='text/css' href='form.css'>        
     </head>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>-->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery.cookie.js"></script>
     <!--<script type="text/javascript" src="jquery.js"></script>-->
     <script type="text/javascript" charset="utf-8">
