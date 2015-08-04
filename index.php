477b2fd51a2ae6760eafc48a1ca8bd94342e2596
David Roesel <roesel@gmail.com>
2015-08-04 16:21:11 -0700

    fixn+1

--- index.php
+++ index.php
@@ -291,8 +291,8 @@ $region_array = array(
                             <div>
                                 <h4>INFO:</h4><p> It will only work if you are placed in Season 5 ranked!
                             </div>
-                            <div style="width:350px;">
-                                <h4>Necessary:</h4><p>...</p>
+                            <div style="width:450px;height:450px;">
+                                
                                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                                 <!-- Lolsigs -->
                                 <ins class="adsbygoogle"
